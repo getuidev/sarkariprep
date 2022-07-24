d64f8700c3e972713aacbf79b9cdc0f0efe1e7f1
pcuiux <98046751+pcuiux@users.noreply.github.com>
2022-07-24 12:28:33 +0530

    toggle js fix

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    $("#toggleNav").click(function () {
+        $("#megaMenu").slideToggle("slow", function () {
+            if ($("#megaMenu").is(":visible")) {
+                $("body").addClass("openMenu");
+            } else {
+                $("body").removeClass("openMenu");
+            }
+        });
+    });
     $(".listItems li").click(function () {
         if ($(this).hasClass("active")) {
             $(".listItems li").removeClass("active")
